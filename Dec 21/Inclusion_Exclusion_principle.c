d5939221f6201e920c489befe635e98f950e3488
Ajay Sharma <ajaysharma388>
2019-12-23 03:41:49 +0530

    update-2019.12.23.3

--- Dec 21/Inclusion_Exclusion_principle.c
+++ Dec 21/Inclusion_Exclusion_principle.c
@@ -14,9 +14,11 @@ int genSubsets(int n,int *arr,int num){
 		n>>=1;
 	}
 	for(int i=0;i<j;++i){
+		printf("%d ", a[i]);
 		prod*=a[i];
 	}
 	int result = floor(num/prod);
+	printf(": %d \n",result);
 	return (j&1)? result : (-1)*result;
 }
 
@@ -28,8 +30,24 @@ int main(int argc, char const *argv[]){
 		scanf("%d",&a[i]);
 	}
 	for(int i=1;i<(1<<n);++i){
-		result+=genSubsets(i,a,num);
+		result+=genSubsets(i,a,num-1);
 	}
-	printf("%d\n", result);
+	printf("Result is : %d\n", result);
 	return 0;
 }
+
+// Sample Input :
+
+// 3 1000
+// 2 3 5
+
+// Sample Output :
+
+// 2 : 499
+// 3 : 333
+// 2 3 : 166
+// 5 : 199
+// 2 5 : 99
+// 3 5 : 66
+// 2 3 5 : 33
+// Result is : 733
